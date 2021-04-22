27b334dd32af0f2da8d2024d3f06f9e676bae96f
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2021-04-22 09:10:07 +0200

    aggiunto filtro di ricerca

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 var app = new Vue({
     el: "#root",
     data: {
-
+        inputFilter: "",
         /* la chiave da popolare col testo inserito dall'utente */
         testo: "",
         /* contenuto di default della lista */
         ulContent: [
-            "Strutturare file HTML",
-            "Stilizzare tramite CSS",
-            "Aggiungere funzionalità JS"
+            {
+                liTxt: "Strutturare file HTML",
+                visible: true
+            },
+            {
+                liTxt: "Stilizzare tramite CSS",
+                visible: true
+            },
+            {
+                liTxt: "Aggiungere funzionalità JS",
+                visible: true
+            }
         ]
     },
 
@@ -25,6 +34,14 @@ var app = new Vue({
         /* rimuove un elemento cliccando sulla X */
         removeTodo(i){
             this.ulContent.splice(i, 1);
+        },
+
+        filterTodos(){
+            for (const obj of this.ulContent) {
+                let lowerTxt = obj.liTxt.toLowerCase();
+                let lowerInput = this.inputFilter.toLowerCase();
+                (lowerTxt.includes(lowerInput)) ? obj.visible = true : obj.visible = false;
+            }
         }
     }
 
